7360a9e995ec33ccfe53408f80d0b0f4e1ca914a
adsfadsf <asdfasdfawewe@asdfasdf.dsaf>
2022-03-15 16:14:11 +0100

    fix navigation

--- public/inc/header.inc.php
+++ public/inc/header.inc.php
@@ -15,17 +15,50 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ?>
 
+<?php
+    function previousPage() {
+
+        $current = $_SERVER["SCRIPT_NAME"];
+
+        if (str_contains($current, "question-01")) {
+            return "index.php";
+        } elseif (str_contains($current, "question-02")) {
+            return "question-01.php";
+        } elseif (str_contains($current, "question-03")) {
+            return "question-02.php";
+        } elseif (str_contains($current, "question-04")) {
+            return "question-03.php";
+        } elseif (str_contains($current, "question-05")) {
+            return "question-04.php";
+        } elseif (str_contains($current, "question-06")) {
+            return "question-05.php";
+        } elseif (str_contains($current, "question-07")) {
+            return "question-06.php";
+        } elseif (str_contains($current, "question-08")) {
+            return "question-07.php";
+        } elseif (str_contains($current, "question-09")) {
+            return "question-08.php";
+        } elseif (str_contains($current, "question-10")) {
+            return "question-09.php";
+        } elseif (str_contains($current, "evaluation")) {
+            return "question-10.php";
+        } else {
+            return "index.php";
+        }
+    }
+?>
+
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Health Survey</a>
+            <a class="navbar-brand" href="index.php">Health Survey</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID"
                 aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarID">
                 <div class="navbar-nav">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    <a class="nav-link active" aria-current="page" href="#">Back</a>
+                    <a class="nav-link" aria-current="page" href="index.php">Home</a>
+                    <a class="nav-link" aria-current="page" href="<?=previousPage();?>">Back</a>
                 </div>
             </div>
         </div>
